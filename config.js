640111bd592bf07a09e739fbb38027e2fbd4498c
wannab3d3v3lop3r <wannab3d3v3lop3r@gmail.com>
2019-03-12 23:20:07 -0400

    updated config endpoints. fixed method in models.js. Added CRUD method to server.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 "use strict";
 
 exports.DATABASE_URL =
-    process.env.DATABASE_URL || "mongodb://localhost/blogPost-app";
-exports.TEST_DATABASE_URL =
-    process.env.TEST_DATABASE_URL || "mongodb://localhost/test-blogPost-app";
-exports.PORT = process.env.PORT || 2164;
+    process.env.DATABASE_URL || "mongodb://localhost/blog";
+exports.PORT = process.env.PORT || 8080;
 
